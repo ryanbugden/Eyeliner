93e8d57e8925b2cdf36ff5ec4e21a8791847c27a
Ryan Bugden <rbugden@gmail.com>
2023-11-29 15:59:12 -0500

    Version up

--- build.py
+++ build.py
@@ -3,7 +3,7 @@
 # -----------------
 
 name                = "Eyeliner"
-version             = "2.8.2"
+version             = "2.8.3"
 developer           = "Ryan Bugden"
 developer_url       = "https://ryanbugden.com"
 rf_version          = "4.4b"
